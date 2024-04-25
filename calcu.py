d91f868780f6737917fb8702eff9aeaa6f8c131a
Joker8832 <ikkayiz123@gmail.com>
2024-04-25 12:05:43 +0530

    New

---
+++ calcu.py
@@ -0,0 +1,5 @@
+from calcufc import*
+add(10,10)
+sub(20,10)
+div(30,5)
+mult(20,10)
---
+++ calcufc.py
@@ -0,0 +1,12 @@
+def add(a,b):
+    c=a+b
+    print("sum:",c)
+def sub(a,b):
+    c=a-b
+    print("sub:",c)
+def mult(a,b):
+    c=a*b
+    print("mult:",c)
+def div(a,b):
+    c=a//b
+    print("div:",c)
