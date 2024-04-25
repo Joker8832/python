68f2f610c63f8a6bad175e165da6303ea176a5eb
Joker8832 <ikkayiz123@gmail.com>
2024-04-25 11:03:17 +0530

    Edited Array

--- 2.py
+++ 2.py
@@ -1,34 +1,34 @@
 name=[]
 def start():
-    select=int(input("select a number 1.Add 2.Select 3.Edit 4.Delete 5.Exit"))
+    select=int(input("select a number (1.Add 2.Select 3.Edit 4.Delete 5.Exit)"))
     if select==1:
-        n=input("Enter A Num")
+        n=input("Enter A Name:")
         name.append(n)
         print(name)
         start()
     elif select==2:
-        n=input("Enter A Selected Name ")
+        n=input("Enter A Selected Name :")
         if n in name:
             print(n)
+            start()
         else:
             name.append(n)
-            print(name)
             data=name.index(n)
-            print("your selected name",name[data])
+            print("your selected name:",name[data])
             start()
     elif select==3:
-        n=input("Enter A Name")
-        m=input("Enter replacing name ")
+        n=input("Enter A Name:")
+        m=input("Enter replacing name :")
         if n in name:
-            print(n)
             data=name.index(n)
-            name[data]=n
+            name[data]=m
             print(name)
+            start()
         else:
-            print(n,"the name is not listed")
+            print(n,"the name is not listed:")
             start()
     elif select==4:
-        n=input("Enter the deleting name")
+        n=input("Enter the deleting name:")
         data=name.index(n)
         del name[data]
         print(name)
