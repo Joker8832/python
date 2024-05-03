8449632904d69fd511182028851760987894d2d6
Joker8832 <ikkayiz123@gmail.com>
2024-05-03 11:19:46 +0530

    calc

--- fhsum.py
+++ fhsum.py
@@ -1,1 +1,9 @@
-file=
+n=int(input("Enter The First Number:"))
+m=int(input("Enter The Second Number:"))
+file=open("fhsum.txt","w")
+sum=n+m
+sub=n-m
+mult=n*m
+div=n//m
+file.write("Sum:"+str(sum)+"\n""Substraction:"+str(sub)+"\n""Multilplication:"+str(mult)+"\n""Division:"+str(div)+"\n""---------------")
+print("Succes")
